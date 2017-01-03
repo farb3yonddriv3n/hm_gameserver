c9de487b365d8de33aeedcdc22c30b89b917e178
Filip Pancik <far.b3yond.driv3n@gmail.com>
2017-01-03 23:47:34 +0100

    Updated: src/game/mechanics/next_turn.c

--- src/game/mechanics/next_turn.c
+++ src/game/mechanics/next_turn.c
@@ -17,56 +17,8 @@
  */
 #include <game.h>
 
-/*
-   static void healing_totem(struct hs_holder_s *p, struct deck_s *deck)
-   {
-   struct card_list_s *c, *cl;
-   struct card_s *found = NULL;
-
-   cl = cards_get_board(deck, NULL, 0, 0);
-
-   for(c = cl; c != NULL; c = c->next) {
-   if(strcmp(c->card->entity->name, "NEW1_009") == 0) {
-   hm_log(LOG_DEBUG, lg, "Healing totem found [%s](%d)", c->card->entity->desc, c->card->id);
-   found = c->card;
-   break;
-   }
-   }
-
-   if(found == NULL) {
-   hm_log(LOG_DEBUG, lg, "Healing totem not found");
-   return;
-   }
-
-   for(c = cl; c != NULL; c = c->next) {
-   entity_attack(p, found, c->card);
-   }
-
-   found->target.card = cl;
-
-   flag(&found->state, MECHANICS_TURN_TRIGGER, FLAG_SET);
-   }
-   */
-
 static void summon_npc(struct conn_client_s *c, struct hs_holder_s *p, struct deck_s *opponent)
 {
-    /*
-       int pos = cards_board_last(opponent);
-
-       hm_log(LOG_DEBUG, lg, "Summon NPC: last position %d", pos);
-
-       if(pos > MAX_BOARD) {
-
-       hm_log(LOG_DEBUG, lg, "Summon NPC: last position %d too high", pos);
-       return;
-       }
-
-       struct card_s *card = add_new_card(p->game, opponent, -1, "CS2_boar", -1);
-       if(card) {
-       mechanics_put_card_onboard(c, pos, card, 1, NULL, NULL, opponent);
-       card->zone_position = pos;
-       }
-       */
     hsl_summon_card(opponent, "EX1_158t", holder);
 }
 
@@ -111,8 +63,6 @@ void mechanics_next_turn(struct conn_client_s *c, struct hs_holder_s *p)
         }
     }
 
-    // healing totem?
-    //healing_totem(p, deck);
     {
         struct deck_s *__fake_deck = deck;
         __SECTION_ENDTURN
