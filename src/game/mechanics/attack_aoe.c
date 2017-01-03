f1181a7f844dde3d83b1fe2dd55f1f1d196e5725
Filip Pancik <far.b3yond.driv3n@gmail.com>
2017-01-03 23:47:01 +0100

    Updated: src/game/mechanics/attack_aoe.c - hero weapon handled in sections - 'fan of knives' code obsolete

--- src/game/mechanics/attack_aoe.c
+++ src/game/mechanics/attack_aoe.c
@@ -44,34 +44,6 @@ void mechanics_attack_aoe(struct conn_client_s *c, u64 position, struct card_s *
         hm_log(LOG_DEBUG, c->log, "Card [%s](%d) heropower not triggered", attacker->entity->desc, attacker->id);
     }
 
-    // hero attack with an equiped weapon
-    if(flag(&attacker->state, CARD_HERO, FLAG_ISSET)) {
-        if(deck->heroweapon) {
-            hm_log(LOG_DEBUG, c->log, "%s Hero %d attacks with a weapon %d", LOG_MECHANICS, attacker->id, deck->heroweapon->id);
-            flag(&(deck->heroweapon->state), MECHANICS_DURABILITY, FLAG_SET);
-            LIST_ADD((*defenders), item, deck->heroweapon);
-
-            if(--(deck->heroweapon->durability) == 0) {
-                attacker->attack = 0;
-                flag(&(attacker->state), MECHANICS_ATTACK, FLAG_SET);
-                flag(&(deck->heroweapon->state), CARD_DESTROYED, FLAG_SET);
-
-                hm_log(LOG_DEBUG, c->log, "%s Hero weapon %d destroyed", LOG_MECHANICS, deck->heroweapon->id);
-
-                deck->heroweapon = NULL;
-            }
-        } else {
-            hm_log(LOG_DEBUG, c->log, "%s Hero %d attacks without a weapon", LOG_MECHANICS, attacker->id);
-        }
-    }
-
-
-    if(flag(&attacker->state, CARD_FAN_OF_KNIVES, FLAG_ISSET)) {
-        struct card_s *card = draw_random_card(deck, p->entity_id, -1);
-        flag(&card->state, CARD_HAND, FLAG_SET);
-        LIST_ADD(*defenders, item, card);
-    }
-
     if(flag(&attacker->state, CARD_SPELL, FLAG_ISSET) && !flag(&attacker->state, CARD_HEROPOWER, FLAG_ISSET)) {
         flag(&attacker->state, CARD_DESTROYED, FLAG_SET);
         cards_reorder(p, deck, attacker, position, 0);
