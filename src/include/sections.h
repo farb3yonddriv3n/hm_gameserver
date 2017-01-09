e165f1471d3c5271416d0cb5ebae76fe1612a323
Filip Pancik <far.b3yond.driv3n@gmail.com>
2017-01-09 19:27:29 +0100

    Updated: src/include/sections.h - added comments - removed duplicated macros

--- src/include/sections.h
+++ src/include/sections.h
@@ -14,10 +14,9 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   */
 #ifndef SECTIONS_H_
 #define SECTIONS_H_
-
 
 #include <section_buff.h>
 #include <section_battlecry.h>
@@ -36,6 +35,7 @@
 #include <section_attachment_attack.h>
 #include <section_attack_effect_attachment.h>
 
+// built-in variables
 #define HSL_attacker (attacker)
 #define HSL_defender (defender)
 #define HSL_defenders (defenders)
@@ -55,16 +55,17 @@
 #define HSL_fake_deck (__fake_deck)
 #define HSL_fake_levelup (&__fake_levelup)
 #define HSL_no_attacker (-1)
-
 #define HSL_heroes_included 1
 #define HSL_heroes_excluded 0
-
 #define HSL_magic_immunity_included 1
 #define HSL_magic_immunity_excluded 0
 
+// generic macros
 #define def(m_type, m_dst) struct m_type *m_dst = NULL;
 
 #define is_spell(m_dst) (flag(&m_dst->state, CARD_SPELL, FLAG_ISSET))
+
+#define cmp_cardname(m_card, m_src)  (strcmp(m_card->entity->name, m_src) == 0)
 
 #define hsl_set_windfury(m_dst)\
     flag(&m_dst->state, CARD_WINDFURY, FLAG_SET)
@@ -75,6 +76,27 @@
 #define hsl_refresh_mana(m_deck, m_amount)\
     deck->mana_used = (m_amount > deck->mana_used) ? 0 : deck->mana_used - m_amount ;
 
+#define turn_plays(m_card) (m_card->turn_plays)
+
+/**
+ * @brief Foreach
+ *
+ * @param m_alias iterator
+ * @param m_src source
+ */
+#define foreach(m_alias, m_src)\
+    for(m_alias = m_src; m_alias != NULL; m_alias = m_alias->next)
+
+/**
+ * @brief Find out if controller is current player
+ *
+ * @param m_card played card
+ */
+#define is_controller_player(m_card)\
+    (p->id == m_card->parent->deck_copy->controller)
+
+// hsl_* macros
+
 // only works on friendly deck
 #define hsl_set_magic_immunity(m_dst)\
     force_expire(p, HSL_deck_player, m_dst);\
@@ -84,8 +106,6 @@ flag(&m_dst->state, CARD_DIVINE_SHIELD, FLAG_SET);
 #define hsl_set_fullhealth(m_dst)\
     m_dst->health = m_dst->total_health;\
 flag(&m_dst->state, MECHANICS_HEALTH, FLAG_SET);
-
-#define card_name(m_dst, m_name) (m_dst && strcmp(m_dst->entity->name, m_name) == 0)
 
 #define hsl_swap_health(m_dst, m_src)\
     int hp = m_dst->health;\
@@ -118,7 +138,6 @@ flag(&m_dst->state, MECHANICS_HEALTH, FLAG_SET);
         flag(&m_dst->state, MECHANICS_HEALTH, FLAG_SET);\
     }
 
-#define turn_plays(m_card) (m_card->turn_plays)
 
 #define hsl_change_armor(m_dst, m_change)\
     m_dst->armor += m_change;\
@@ -253,8 +272,6 @@ for(m_holder = m_all; m_holder != NULL; m_holder = m_holder->next) {\
 
 #define card(m_list) (m_list->card)
 
-#define cardname_equal(m_dst, m_src) (strcmp((m_dst->entity->name), m_src) == 0)
-
 #define hsl_card(m_target)\
     card_get(HSL_deck_player, HSL_deck_opponent, m_target)
 
@@ -281,6 +298,13 @@ for(m_holder = m_all; m_holder != NULL; m_holder = m_holder->next) {\
 #define hsl_attack_aoe(m_dst, m_src, m_fake_levelup) mechanics_attack_aoe(c, opt->position, m_dst, &m_src, m_fake_levelup)
 #define hsl_attack(m_dst, m_src, m_fake_levelup) hsl_attack_aoe(m_dst, m_src, m_fake_levelup)
 
+/**
+ * @brief Summon card for deck
+ *
+ * @param m_deck deck to summon to
+ * @param m_name name of the card
+ * @param m_card card holder
+ **/
 #define hsl_summon_card(m_deck, m_name, m_card)\
     int m_card##pos = cards_board_last(m_deck);\
 if(m_card##pos <= MAX_BOARD) {\
@@ -288,10 +312,28 @@ if(m_card##pos <= MAX_BOARD) {\
     mechanics_put_card_onboard(c, m_card##pos, m_card, 1, NULL, NULL, m_deck);\
 }
 
+/**
+ * @brief Count number of cards in list of cards
+ *
+ * @param m_cards list of cards
+ **/
 #define hsl_count_cards(m_cards) (cards_count(m_cards))
 
+/**
+ * @brief Get random card from list of cards
+ *
+ * @param m_cards list of cards
+ **/
 #define hsl_random_card(m_cards) (card_random(m_cards))
 
+/**
+ * @brief Draw number of cards from player's deck
+ *
+ * @param m_player deck owner
+ * @param m_deck deck to draw from
+ * @param m_card card's holder
+ * @param m_amount number of cards to draw
+ **/
 #define hsl_draw_cards(m_player, m_deck, m_card, m_amount)\
     int m_card##idx;\
 struct card_s *m_card;\
@@ -304,99 +346,193 @@ for(m_card##idx = 0; m_card##idx < m_amount; m_card##idx++) {\
     }\
 }
 
-#define hsl_cmp_cardname(m_dst, m_src)  (strcmp(m_dst->card->entity->name, m_src) == 0)
-
+/**
+ * @brief Link card to another card's targets
+ *
+ * @param m_dst destination card
+ * @param m_src source card
+ **/
 #define hsl_link_target(m_dst, m_src)\
     LIST_ADD_LINK(m_dst->target.card, m_src)
 
+/**
+ * @brief Add target to defenders
+ *
+ * @param m_src source card
+ * @param m_holder variable placeholder
+ **/
 #define hsl_add_target(m_src, m_holder)\
     struct card_list_s *m_holder##item;\
 LIST_ADD(defenders, m_holder##item, m_src);\
 
+/**
+ * @brief Attack as battlecry
+ *
+ * @param m_attacker attacking card
+ * @param m_defender defending card
+ * @param m_damage damage caused
+ **/
 #define hsl_attack_bc(m_attacker, m_defender, m_damage)\
     m_defender->receive.damage = m_damage;\
 entity_attack(p, m_attacker, m_defender);
 
+/**
+ * @brief Heal defender by certain amount (alias)
+ **/
 #define hsl_heal_bc(m_attacker, m_defender, m_damage)\
-    m_defender->receive.heal = m_damage;\
-entity_attack(p, m_attacker, m_defender);\
-struct card_list_s *item;\
+    hsl_heal(m_attacker, m_defender, m_damage);
+/*
+   m_defender->receive.heal = m_damage;\
+#entity_attack(p, m_attacker, m_defender);\
+#struct card_list_s *item;\
 LIST_ADD(m_attacker->target.card, item, m_defender);
+*/
 
+/**
+ * @brief Heal defender by certain amount
+ *
+ * @param m_attacker healer card
+ * @param m_defender receiver card
+ * @param m_damage amount of healing
+ **/
 #define hsl_heal(m_attacker, m_defender, m_damage)\
     m_defender->receive.heal = m_damage;\
 entity_attack(p, m_attacker, m_defender);\
 struct card_list_s *item;\
 LIST_ADD(m_attacker->target.card, item, m_defender);
 
-#define controller_player(m_card)\
-    (p->id == m_card->parent->deck_copy->controller)
-
-#define foreach(m_alias, m_src)\
-    for(m_alias = m_src; m_alias != NULL; m_alias = m_alias->next)
-
+/**
+ * @brief Mark target as corrupted
+ *
+ * Corrupted target is destroyed after m_turn turns
+ *
+ * @param m_turn number of turns from current turn when card is destroyed
+ * @param m_defender destination card
+ */
 #define hsl_buff_corrupt_expire(m_turn, m_defender)\
     flag(&(m_defender->state), CARD_CORRUPTED, FLAG_SET);\
 mechanics_expire(p->game->turn + m_turn, m_defender, EXPIRE_DESTROY, 1, NULL)
 
+/**
+ * @brief Change attack for m_turn turns
+ *
+ * @param m_turn expiration turn
+ * @param m_defender destination card
+ * @param m_attack value to change
+ */
 #define hsl_buff_attack_expire(m_turn, m_defender, m_attack)\
     m_defender->attack += m_attack;\
 flag(&(m_defender->state), MECHANICS_ATTACK, FLAG_SET);\
-mechanics_expire(p->game->turn + m_turn, m_defender, EXPIRE_ATTACK, -(m_attack), NULL)
+mechanics_expire(p->game->turn + m_turn, m_defender, EXPIRE_ATTACK, -(m_attack), NULL);
 
+/**
+ * @brief Change health of card
+ *
+ * @param m_card destination card
+ * @param m_health health value
+ */
 #define hsl_add_attachment(m_player, m_attacker, m_defender, m_turn)\
     add_single_attachment(m_player, hsl_deck(m_defender), m_attacker, m_defender);\
 mechanics_expire(p->game->turn + m_turn, m_defender, EXPIRE_ATTACHMENT, 0, m_attacker->attachment);
 
+/**
+ * @brief Change health of card
+ *
+ * @param m_card destination card
+ * @param m_health health value
+ */
 #define buff_health(m_card, m_health)\
     defenders->card->health = m_health;\
 flag(&(m_card->state), MECHANICS_HEALTH, FLAG_SET);
 
+/**
+ * @brief Set divine shield to card
+ *
+ * @param m_card destination card
+ */
 #define hsl_divine_shield(m_card)\
     flag(&(m_card->state), CARD_DIVINE_SHIELD, FLAG_SET);
 
+/**
+ * @brief Set flag to card
+ *
+ * @param m_card destination card
+ * @param m_flag flag to set
+ */
 #define setflag(m_card, m_flag)\
     flag(&(m_card->state), m_flag, FLAG_SET);
 
+/**
+ * @brief Unset flag from card
+ *
+ * @param m_card destination card
+ * @param m_flag flag to unset
+ */
 #define unsetflag(m_card, m_flag)\
     flag(&(m_card->state), m_flag, FLAG_UNSET);
 
+/**
+ * @brief Unset windfury flag from card
+ *
+ * @param m_card destination card
+ */
 #define hsl_unset_windfury(m_card) (flag(&(m_card->state), CARD_WINDFURY, FLAG_UNSET))
 
+/**
+ * @brief Set exhausted flag to card
+ *
+ * @param m_card destination card
+ */
 #define hsl_set_exhausted(m_card) (flag(&(m_card->state), CARD_EXHAUSTED, FLAG_SET))
 
+/**
+ * @brief Unset exhausted flag from card
+ *
+ * @param m_card destination card
+ */
 #define hsl_unset_exhausted(m_card) (flag(&(m_card->state), CARD_EXHAUSTED, FLAG_UNSET))
 
+/**
+ * @brief Transform defender to m_name
+ *
+ * Any mechanics like polymorph
+ *
+ * @param m_attacker card with ability to transform target
+ * @param m_defender card being attacked
+ * @param m_name card code name
+ */
 #define hsl_transform(m_attacker, m_defender,m_name)\
     struct card_s *card;\
 card = add_new_card(p->game, p->opponent.deck_copy, -1, m_name, -1);\
 card->zone_position = m_defender->zone_position;\
 mechanics_put_card_onboard(c, card->zone_position, card, 1, NULL, NULL);\
-\
 m_defender->linked = card->id;\
 flag(&m_defender->state, CARD_LINKED, FLAG_SET);\
 flag(&m_defender->state, CARD_HIDE, FLAG_SET);\
 flag(&m_defender->state, CARD_BOARD, FLAG_UNSET);
 
+/**
+ * @brief Give weapon to player
+ *
+ * Called only in '.section onboard'. There is no need to linked destroyed weapon to defenders
+ *
+ * @param m_dst hero card
+ */
 #define hsl_give_weapon(m_dst)\
-    /* this is onboard action, no need to link destroyed wepon to defenders */\
-if(m_dst->heroweapon != NULL) {\
-    /* destroy weapon first */\
-    {\
-        struct card_s *__fake_weapon = m_dst->heroweapon;\
-        __SECTION_WEAPON_DESTROYED;\
+    if(m_dst->heroweapon != NULL) {\
+        /* destroy weapon first */\
+        {\
+            struct card_s *__fake_weapon = m_dst->heroweapon;\
+            __SECTION_WEAPON_DESTROYED;\
+        }\
+        \
+        m_dst->heroweapon->priority_override = 900;\
+        flag(&(m_dst->heroweapon->state), CARD_DESTROYED, FLAG_SET);\
+        flag(&(m_dst->heroweapon->state), CARD_BOARD, FLAG_UNSET);\
+        flag(&(m_dst->heroweapon->state), CARD_HEROWEAPON, FLAG_UNSET);\
     }\
-    \
-    m_dst->heroweapon->priority_override = 900;\
-    flag(&(m_dst->heroweapon->state), CARD_DESTROYED, FLAG_SET);\
-    flag(&(m_dst->heroweapon->state), CARD_BOARD, FLAG_UNSET);\
-    flag(&(m_dst->heroweapon->state), CARD_HEROWEAPON, FLAG_UNSET);\
-}\
 flag(&attacker->state, CARD_BOARD, FLAG_SET);\
 flag(&attacker->state, CARD_HEROWEAPON, FLAG_SET);\
 m_dst->heroweapon = attacker;
-/*m_dst->hero->attack = attacker->attack;\
-  flag(&m_dst->hero->state, MECHANICS_ATTACK, FLAG_SET);*/
-
 
 #endif
